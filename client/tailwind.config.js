6aa56e27a4eca61beafc81e9f24c627461bb4b7a
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2025-03-29 11:57:11 +0530

    Initial client structure

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-750': 'rgb(40, 44, 52)',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
